b9865ac727196e1d85d4f0eb05acb50f10020ba0
Alex Verbitsky <alexandruverbitsky@alexs-air.home>
2023-07-19 14:35:22 -0400

    Splitting Components Into Multiple Components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import './ExpenseItem.css'
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem({ date, title, amount }) {
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate
+        date={date}
+      ></ExpenseDate>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

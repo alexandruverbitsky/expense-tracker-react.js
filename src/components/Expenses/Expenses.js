8a543dc3eef7ac348105b3509e27652b11f1813e
Alex Verbitsky <alexandruverbitsky@alexs-air.home>
2023-07-21 13:50:48 -0400

    add expenseList

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
 import Card from '../UI/Card';
 import { useState } from 'react';
+import ExpensesList from './ExpensesList';
 
 const years = ["2023", "2022", "2021", "2020", "2019", "2018"];
 
@@ -18,31 +19,18 @@ const Expenses = ({ expenses }) => {
   const filteredExpenses = expenses
     .filter((item) => item.date.getFullYear().toString() === filteredYear);
 
-
-  return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter
-          filterYears={years}
-          selectedYear={filteredYear}
-          onYearFilterChange={yearFilterHandler}
-        />
-        {filteredExpenses.length === 0 && (
-          <p>No expenses found</p>
-        )}
-        {filteredExpenses.length > 0 && 
-          filteredExpenses.map((item) => (
-            <ExpenseItem
-              key={item.id}
-              date={item.date}
-              title={item.title}
-              amount={item.amount}
-            />
-          ))
-        }
-      </Card>
-    </div>
-  );
+    return (
+      <div>
+        <Card className="expenses">
+          <ExpensesFilter
+            filterYears={years}
+            selectedYear={filteredYear}
+            onChonYearFilterChangeangeFilter={yearFilterHandler}
+          />
+          <ExpensesList items={filteredExpenses} />
+        </Card>
+      </div>
+    );
 }
 
 export default Expenses;
